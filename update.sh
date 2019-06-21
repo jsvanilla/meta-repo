f77d545bfa83e66e65fa84f48296a7cea84e5ab7
Kelly Sovacool <kellysovacool@gmail.com>
2019-06-21 10:22:34 -0400

    Only commit changes if update succeeds

--- update.sh
+++ update.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-source ~/.bash_profile
-conda activate git-repos
-python git-repos.py -t .token
-git add .
-git commit -m "Auto-update repos"
-git push
+
+function update() {
+    source ~/.bash_profile
+    conda activate git-repos
+    python git-repos.py -t .token
+}
+
+function commit() {
+    git add .
+    git commit -m "Auto-update repos"
+    git push
+}
+
+if update; then
+    commit
+else
+    echo "* Update failed! *"
+fi
