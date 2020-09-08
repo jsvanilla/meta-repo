10963b8e5a51c488815be8480df019199f56324d
Kelly Sovacool <kellysovacool@gmail.com>
2020-09-08 12:28:16 -0400

    Tweak plots

--- code/plot_language_stats.R
+++ code/plot_language_stats.R
@@ -5,64 +5,105 @@ theme_set(theme_classic())
 update_geom_defaults("bar", list(fill = RColorBrewer::brewer.pal(n=9,name="GnBu")[9]))
 theme_update(axis.text.x = element_text(angle = 45, hjust = 1))
 
-plot_bar <- function(data, x_col = language, y_col = total_bytes, 
-                     xlab_str ='', ylab_str='', title_str='', filename = NULL,
-                     aspect_ratio = 4/3, height = 6, width = NULL, units = "in") {
-    plot <- data %>%
+plot_bar <- function(data,
+                     x_col = language,
+                     y_col = total_bytes,
+                     xlab_str = '',
+                     ylab_str = '',
+                     title_str = '') {
+    
+    return( data %>%
         ggplot2::ggplot(aes(x = {{ x_col }}, 
                             y = {{ y_col }}, 
                             label = {{ y_col }})) +
-        geom_bar(stat="identity")+#, fill="#0868AC") +
-        geom_text(aes(label={{ y_col }}), vjust=-0.5, size=3) +
-        xlab(xlab_str) +
-        ylab(ylab_str) +
-        ggtitle(title_str)
-    
-    if (is.null(width)) {
-        width <- height * aspect_ratio
-    }
+            geom_bar(stat="identity")+
+            geom_text(aes(label={{ y_col }}), vjust = -0.4, size = 3) +
+            scale_y_continuous(
+                breaks = c(100000, 250000, 500000, 750000, 1000000),
+                labels = c('100k', '250k', '500k', '750k', '1mil')
+            ) +
+            xlab(xlab_str) +
+            ylab(ylab_str) +
+            #coord_flip() +
+            ggtitle(title_str)
+    )
+}
+write_plot <- function(plot,
+                           filename = NULL,
+                           aspect_ratio = 4 / 3,
+                           height = 6,
+                           width = NULL,
+                           units = "in") {
+        
+        if (is.null(width)) {
+            width <- height * aspect_ratio
+        }
     if (!is.null(filename)) {
-        ggsave(here::here("figures", filename),
-               height=height, width=width, units=units)
+        ggsave(
+            here::here("figures", filename),
+            plot = plot,
+            height = height,
+            width = width,
+            units = units
+        )
     }
-    return(plot)
 }
 
+
 sum_lang_data <- function(data) {
-    return(data %>%
-               filter(!(repo_name %in% c("ML_pipeline_microbiome", 
-                                         "caret", 
-                                         "CluMSID"))) %>%
-               filter(!(repo_owner_name %in% c("JMAStough", 
-                                               "akhagan"))) %>%
-               group_by(language) %>%
-               summarize(total_bytes=sum(language_repo_bytes),
-                         repo_count=n())
+    return(
+        data %>%
+            filter(!(
+                repo_name %in% c("caret",
+                                 "CluMSID")
+            )) %>%
+            filter(!(
+                repo_owner_name %in% c("JMAStough",
+                                       "akhagan")
+            )) %>%
+            group_by(language) %>%
+            summarize(
+                total_bytes = sum(language_repo_bytes),
+                repo_count = n()
+            )
     )
 }
 
-data_raw <- readr::read_csv(here::here("data", "repo_languages.csv"))
+
+data_raw <-
+    readr::read_csv(here::here("data", "repo_languages.csv"))
 data_sum <- sum_lang_data(data_raw) %>%
-    filter(!(language %in% c("HTML", "CSS", "Limbo"))) %>% 
-    mutate(language=reorder(language, -total_bytes))
+    filter(!(language %in% c("HTML", "CSS", "Limbo"))) %>%
+    mutate(language = reorder(language, -total_bytes))
 
-plot_all_bytes <- data_sum %>% plot_bar( 
-                           x_col = language, y_col = total_bytes, 
-                           ylab_str = "bytes of code", 
-                           title_str = "My languages by bytes of code on GitHub", 
-                           filename = "language_all_bytes.png")
+# all bytes
+data_sum %>%
+    plot_bar(
+        x_col = language,
+        y_col = total_bytes,
+        ylab_str = "bytes of code",
+        title_str = "My languages by bytes of code on GitHub"
+    ) %>%
+    write_plot(filename = "language_all_bytes.png")
 
-plot_bytes_5 <- plot_bar(data_sum %>% top_n(5, total_bytes), 
-                         x_col = language, y_col = total_bytes, 
-                         ylab_str = "bytes of code", 
-                         title_str = "My top languages on GitHub", 
-                         filename = "language_all_bytes_n5.png", 
-                         height = 4, width = 4)
+# bytes top 6
+data_sum %>% top_n(6, total_bytes) %>%
+    plot_bar(
+        x_col = language,
+        y_col = total_bytes,
+        ylab_str = "bytes of code",
+        title_str = "My top languages on GitHub"
+    ) %>% write_plot(filename = "language_all_bytes_n6.png",
+                     height = 4,
+                     width = 4)
 
-plot_all_repos <- plot_bar(data_sum %>% 
-                               mutate(language=reorder(language, -repo_count)),
-                           x_col = language, y_col = repo_count,
-                           ylab_str = "# of repos", 
-                           title_str = "My languages by presence in GitHub repositories", 
-                           filename = "language_all_repos.png")
-
+# all repos
+data_sum %>%
+    mutate(language = reorder(language, -repo_count)) %>%
+    plot_bar(
+        x_col = language,
+        y_col = repo_count,
+        ylab_str = "# of repos",
+        title_str = "My languages by presence in GitHub repositories"
+    ) %>% 
+    write_plot(filename = "language_all_repos.png")
